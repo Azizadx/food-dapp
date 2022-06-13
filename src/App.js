5260cc2da125073a2e2bf10a958751d7f60c3269
Azizadx <nasrallahh55@gmail.com>
2022-06-13 20:55:44 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useCallback, useState } from "react";
+import { Container, Nav } from "react-bootstrap";
+import { login, logout as destroy, accountBalance } from "./utils/near";
+import Wallet from "./components/wallet";
+import { Notification } from "./components/utils/Notifications";
+import Products from "./components/marketplace/Products";
+import Cover from "./components/utils/cover";
+import coverImg from "/Users/azizamed/Documents/Development/near-blockchain/learning/near-marketplace-ui/src/asset/img/sandwich.jpg";
+import "./App.css";
 
-function App() {
+const App = function AppWrapper() {
+  const account = window.walletConnection.account();
+  const [balance, setBalance] = useState("0");
+
+  useEffect(() => {
+    const getBalance = async function getBalance() {
+      if (account.accountId) {
+        setBalance(await accountBalance());
+      }
+    };
+    getBalance();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Notification />
+      {account.accountId ? (
+        <Container fluid="md">
+          <Nav className="justify-content-end pt-3 pb-5">
+            <Nav.Item>
+              <Wallet
+                address={account.accountId}
+                amount={balance}
+                symbol="NEAR"
+                destroy={destroy}
+              />
+            </Nav.Item>
+          </Nav>
+          <main><Products /></main>
+        </Container>
+      ) : (
+        <Cover name="Street Food" login={login} coverImg={coverImg} />
+      )}
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeContract } from "./utils/near";
+import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+window.nearInitPromise = initializeContract()
+  .then(() => {
+    const root = ReactDOM.createRoot(
+      document.getElementById("root")
+    )
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    // ReactDOM.render(
+    //   <React.StrictMode>
+    //     <App />
+    //   </React.StrictMode>,
+    //   document.getElementById("root")
+    // );
+  })
+  .catch(console.error);
+
 reportWebVitals();
