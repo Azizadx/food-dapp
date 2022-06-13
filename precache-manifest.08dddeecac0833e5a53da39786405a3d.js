6839ebbd5c917f2cf4cb2b82eddca174b151003e
Azizadx <nasrallahh55@gmail.com>
2022-06-13 21:05:09 +0300

    Updates

---
+++ precache-manifest.08dddeecac0833e5a53da39786405a3d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5b89d850bc6c03f4f58",
+    "url": "/food-dapp/static/css/main.9e38bd6c.chunk.css"
+  },
+  {
+    "revision": "e5b89d850bc6c03f4f58",
+    "url": "/food-dapp/static/js/main.41a8b937.chunk.js"
+  },
+  {
+    "revision": "013cdbf22a44618df3b9",
+    "url": "/food-dapp/static/js/runtime~main.5dcf2bcc.js"
+  },
+  {
+    "revision": "462ccc2caedae4d48f21",
+    "url": "/food-dapp/static/css/2.0ac71c44.chunk.css"
+  },
+  {
+    "revision": "462ccc2caedae4d48f21",
+    "url": "/food-dapp/static/js/2.de92b97b.chunk.js"
+  },
+  {
+    "revision": "7773045e9f78645e32d1",
+    "url": "/food-dapp/static/js/3.86c40c03.chunk.js"
+  },
+  {
+    "revision": "bc90abf1d28c6c9af1d0bce7b7e13aef",
+    "url": "/food-dapp/static/media/sandwich.bc90abf1.jpg"
+  },
+  {
+    "revision": "a6a4862c8f0a585c6c1f6d5aa07b761c",
+    "url": "/food-dapp/static/media/bootstrap-icons.a6a4862c.woff"
+  },
+  {
+    "revision": "b57a58905370284f1499d1a35f5d2c96",
+    "url": "/food-dapp/static/media/bootstrap-icons.b57a5890.woff2"
+  },
+  {
+    "revision": "337146e34b4627ce33b48674d88b7b6d",
+    "url": "/food-dapp/index.html"
+  }
+];
